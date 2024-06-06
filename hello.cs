49541105df9cf16f0ae7baf2d56d4ded016e0cf8
Khadeeja Beevi C N <khadeejabeevinavas@gmail.com>
2024-06-06 10:05:53 +0530

    committed

---
+++ hello.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class program1
+{
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("khadeeja");
+            Console.WriteLine("Nintu");
+        Console.WriteLine("Nivya");
+    }
+    
+}
+
