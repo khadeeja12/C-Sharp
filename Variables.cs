0c50c40785877b1a858c64ab8a2ed0387392d813
Khadeeja Beevi C N <khadeejabeevinavas@gmail.com>
2024-06-06 10:30:18 +0530

    commit

---
+++ Variables.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace dt2
+{
+    internal class Program
+    {
+        static void Main(string[]args)
+        {
+            int mynum = 5;
+            double myDoubleNum = 5.99;
+            char myLetter = 'K';
+            bool myBool = true;
+            string myText = "Hello";
+            Console.WriteLine(mynum);
+            Console.WriteLine(myDoubleNum);
+            Console.WriteLine(myLetter);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myText);
+
+        }
+    }
+}
