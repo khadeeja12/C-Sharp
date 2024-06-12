ac1c759ed84a83909a6e80eb1181a8f2b64e6e1d
Khadeeja Beevi C N <khadeejabeevinavas@gmail.com>
2024-06-12 09:53:06 +0530

    Update Switch.cs

--- Switch.cs
+++ Switch.cs
@@ -1,15 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 
-public class program1
+namespace evenodd
 {
-    
+    class OddorEven
+    {
         static void Main(string[] args)
         {
-            Console.WriteLine("khadeeja");
-            Console.WriteLine("Nintu");
-            Console.WriteLine("Nivya");
-    }
-    
-}
+            Console.WriteLine("Enter a day between 1-7");
+            string input = Console.ReadLine();
+            int day;
 
+            // Validate and parse the input
+            if (int.TryParse(input, out day) && day >= 1 && day <= 7)
+            {
+                switch (day)
+                {
+                    case 1:
+                        Console.WriteLine("Day is Monday");
+                        break;
+                    case 2:
+                        Console.WriteLine("Day is Tuesday");
+                        break;
+                    case 3:
+                        Console.WriteLine("Day is Wednesday");
+                        break;
+                    case 4:
+                        Console.WriteLine("Day is Thursday");
+                        break;
+                    case 5:
+                        Console.WriteLine("Day is Friday");
+                        break;
+                    case 6:
+                        Console.WriteLine("Day is Saturday");
+                        break;
+                    case 7:
+                        Console.WriteLine("Day is Sunday");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid day entered");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number between 1 and 7.");
+            }
+        }
+    }
+}
