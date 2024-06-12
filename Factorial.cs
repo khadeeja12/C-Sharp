3f87e4e016fb5c8f4e5bfe5295f275bcbc965be1
Khadeeja Beevi C N <khadeejabeevinavas@gmail.com>
2024-06-12 09:35:51 +0530

    Update Factorial.cs

--- Factorial.cs
+++ Factorial.cs
@@ -6,9 +6,15 @@ public class program1
     
         static void Main(string[] args)
         {
-            Console.WriteLine("khadeeja");
-            Console.WriteLine("Nintu");
-            Console.WriteLine("Nivya");
+            int i, fact = 1, number;
+            Console.WriteLine("Enter any number:");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of " + number + " is : " + fact);
+
     }
     
 }
